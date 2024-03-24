dbcdd3a3fd14f848d88a9aac4b4d2d2d7b2406a3
Lev Ustimenko <Leva3175@gmail.com>
2024-03-25 00:21:29 +0300

    Another implementation of deletion (via bean method names)

--- src/main/java/ru/levandr/FourLifeCycleBean/AppConfig.java
+++ src/main/java/ru/levandr/FourLifeCycleBean/AppConfig.java
@@ -1,7 +1,12 @@
 package ru.levandr.FourLifeCycleBean;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("ru.levandr.FourLifeCycleBean")
 public class AppConfig {
+    @Bean(initMethod = "afterInit", destroyMethod = "beforeDestroy")
+    public SimpleBean simpleBean(){
+        return new SimpleBean();
+    }
 }
--- src/main/java/ru/levandr/FourLifeCycleBean/Main.java
+++ src/main/java/ru/levandr/FourLifeCycleBean/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         SimpleBean simpleBean = context.getBean(SimpleBean.class);
 
-        ((AbstractApplicationContext)context).getBeanFactory().destroyBean(simpleBean);
+        ((AbstractApplicationContext)context).getBeanFactory().destroyBean("simpleBean",simpleBean);
 
     }
 
