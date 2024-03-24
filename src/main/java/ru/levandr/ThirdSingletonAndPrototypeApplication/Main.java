573cce1a3491d9aaeeed70e180cb6ae3b2a945e0
Lev Ustimenko <Leva3175@gmail.com>
2024-03-24 14:34:40 +0300

    refactor main

--- src/main/java/ru/levandr/ThirdSingletonAndPrototypeApplication/Main.java
+++ src/main/java/ru/levandr/ThirdSingletonAndPrototypeApplication/Main.java
@@ -6,17 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        ObjectFactoryHolder factoryHolder = applicationContext.getBean(ObjectFactoryHolder.class);
-
-        factoryHolder.getPrototype().printCreatedTime();
-        factoryHolder.getSingleton().printCreatedTime();
-
-        System.out.println("-----------------");
-
-        System.out.println(factoryHolder.getPrototype() == factoryHolder.getPrototype());
-
-        System.out.println(factoryHolder.getSingleton() == factoryHolder.getSingleton());
-
+        System.out.println("Main call");
+        applicationContext.getBean(SecondSingleton.class);
     }
 }
